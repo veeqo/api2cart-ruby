adefcf8051988c626dfbbb3f72e16696cd5eb295
Daniel Vartanov <dan@vartanov.net>
2014-11-19 12:52:12 +0000

    Soften version requirement for ActiveSupport

--- lib/api2cart/request_url_composer.rb
+++ lib/api2cart/request_url_composer.rb
@@ -1,5 +1,5 @@
 require 'uri'
-require 'active_support/core_ext/object/to_param'
+require 'active_support/core_ext/object/to_query'
 
 module Api2cart
   class RequestUrlComposer < Struct.new(:api_key, :store_key, :method_name, :query_params)
@@ -21,7 +21,7 @@ module Api2cart
     end
 
     def query_string
-      { api_key: api_key, store_key: store_key }.merge(query_params || {}).to_query
+      { api_key: api_key, store_key: store_key }.merge(query_params || {}).to_param
     end
   end
 end
